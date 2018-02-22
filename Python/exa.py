77327c54e895e05b414d588395b17888be72fade
JulesMalard30 <julesmalard@aol.com>
2018-02-22 15:14:23 +0100

    Update exa.py / Segmentation with vector

--- Python/exa.py
+++ Python/exa.py
@@ -4,26 +4,44 @@ import sys
 import numpy as np
 import datetime
 
-FILENAME = "conefilenonoise.las" 
-
-#Lecture fichier las + ajout VLR
-inFile = laspy.file.File(FILENAME, mode= "r")
-oldVlrs = copy.copy(inFile.header.vlrs)
-vlr = laspy.header.VLR("Exametrics",1, "\x00" * 0, description = "las file modified")
-oldVlrs.append(vlr)
+PAS = 0.001
 
 #Recuperation argument
 try:
-    axe = int(sys.argv[1])
-except ValueError:
-    print("Erreur axe invalide")
-    sys.exit(0) 
+    fileName = sys.argv[1]
 except IndexError:
     print("Erreur axe invalide")
-    sys.exit(0)     
+    sys.exit(0)  
     
 try:
-    offset = float(sys.argv[4])
+    vectorA = float(sys.argv[2))
+except ValueError:
+    print("Erreur vectorA invalide")
+    sys.exit(0) 
+except IndexError:
+    print("Erreur vectorA invalide")
+    sys.exit(0) 
+        
+try:
+    vectorB = float(sys.argv[3])
+except ValueError:
+    print("Erreur vectorB invalide")
+    sys.exit(0) 
+except IndexError:
+    print("Erreur vectorB invalide")
+    sys.exit(0) 
+    
+try:
+    vectorC = float(sys.argv[4])
+except ValueError:
+    print("Erreur vectorC invalide")
+    sys.exit(0) 
+except IndexError:
+    print("Erreur vectorC invalide")
+    sys.exit(0)    
+    
+try:
+    offset = float(sys.argv[5])
 except ValueError:
     print("Erreur offset invalide")
     sys.exit(0) 
@@ -32,43 +50,43 @@ except IndexError:
     sys.exit(0) 
  
 try:
-    marge = float(sys.argv[5])
+    marge = float(sys.argv[6])
 except IndexError:
     marge = 0
 except ValueError:
     marge = 0
-
+    
+#Lecture fichier las + ajout VLR
+inFile = laspy.file.File(fileName, mode= "r")
+oldVlrs = copy.copy(inFile.header.vlrs)
+vlr = laspy.header.VLR("Exametrics",1, "\x00" * 0, description = "las file modified")
+oldVlrs.append(vlr)
+    
 nbrPoint = len(inFile.points)
 minValue = offset - marge
 maxValue = offset + marge
+
 tmp = 0
 tabX = []
 tabY = []
 tabZ = []
 tabG = []
+offsetTmp = 0
+float(offsetTmp)
 while tmp < nbrPoint:
     dataX = inFile.x[tmp]
     dataY = inFile.y[tmp]
     dataZ = inFile.z[tmp]
     GPS = inFile.gps_time[tmp]
-    if axe == 1:
-        if dataX > minValue and dataX < maxValue:
+    offsetTmp = minValue
+    while offsetTmp < maxValue:
+        equation = dataX * vectorA + dataY * vectorB + dataZ * vectorC + offsetTmp
+        if equation == 0:
             tabX.append(dataX)
             tabY.append(dataY)
             tabZ.append(dataZ)
             tabG.append(GPS)
-    elif axe == 2:
-        if dataY > minValue and dataY < maxValue:
-            tabX.append(dataX)
-            tabY.append(dataY)
-            tabZ.append(dataZ)       
-            tabG.append(GPS)
-    else:
-        if dataZ > minValue and dataZ < maxValue:
-            tabX.append(dataX)
-            tabY.append(dataY)
-            tabZ.append(dataZ)     
-            tabG.append(GPS)
+        offsetTmp = offsetTmp + PAS
     tmp = tmp + 1
     print(tmp*100/nbrPoint)
 
@@ -78,7 +96,7 @@ allz = np.array(tabZ)
 allg = np.array(tabG)
 
 now = datetime.datetime.now()
-newFileName = now.strftime("%Y_%m_%d_%H_%M_") + FILENAME
+newFileName = now.strftime("%Y_%m_%d_%H_%M_") + fileName
 
 #Sauvegarde et fermeture fichier las
 outFile = laspy.file.File(newFileName,  mode= "w", vlrs = oldVlrs, header = inFile.header)
